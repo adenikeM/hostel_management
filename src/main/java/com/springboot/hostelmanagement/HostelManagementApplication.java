1556362ff15b2a96f0af22f36247ebca4d652c52
Moronkola Adijat <horythorkeh@gmail.com>
2023-12-31 19:44:05 +0100

    commit

---
+++ src/main/java/com/springboot/hostelmanagement/HostelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hostelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelManagementApplication.class, args);
+    }
+
+}
